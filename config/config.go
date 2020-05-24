dd4850444bda237ca9c49200707b1734751e3680
B'BEAM <yinglae@gmail.com>
2020-05-24 16:56:36 +0700

    change domain

--- config/config.go
+++ config/config.go
@@ -3,4 +3,4 @@ package config
 const Username = "barnarhan@gmail.com"
 const Password = "p@ssWoRD"
 const Hostname = "smtp.gmail.com"
-const Domain  = "https://cooking-server.herokuapp.com"
+const Domain  = "https://cooking-server.herokuapp.com:9000"
